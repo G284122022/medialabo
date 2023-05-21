ebc43a54aeb578cca7aadf8ed1d3717287a9d2cf
G284122022 <105191625+G284122022@users.noreply.github.com>
2023-05-21 12:18:20 +0900

    project.htmlにproject.jsを組み込む

--- weather.js
+++ weather.js
@@ -46,19 +46,9 @@ let data = {
 };
 
 ////////// 課題3-2 ここからプログラムを書こう
-let elem = document.querySelector("input#mytext");
-elem.setAttribute("placeholder","入力してね！");
-let cn = document.querySelector("li#cn");
-cn.setAttribute("placeholder","データを表示");
+console.log(data.name);
 
-let pcn1 = document.createElement("p");
-pcn1.textContent = data.name;
-cn.insertAdjacentElement("afterend",pcn1);
+console.log(data.main.temp_max);
 
-let pcn2 = document.createElement("p");
-pcn2.textContenct = data.main.temp_max;
-pcn1.insertAdjacentElement("afterend",pcn2);
+console.log(data.main.temp_min);
 
-let pcn3 = document.createElement("p");
-pcn3.textContent = data.main.temp_min;
-pcn2.insertAdjacentElement("afterend",pcn3);
