9980b6f17f81069f88810f5e9a1c55f1e42bc2bd
G284122022 <105191625+G284122022@users.noreply.github.com>
2023-05-24 10:48:18 +0900

    変更内容を失わないために一応プッシュ

--- kazuate.js
+++ kazuate.js
@@ -8,13 +8,14 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-
-
+let answers = document.querySelector("button#answers");
+answers.addEventListener("click", hantei);
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let i = document.querySelector('input[name="nyuryoku"]');
+  let j = i.value;
+  let yoso = parseInt(j);
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
