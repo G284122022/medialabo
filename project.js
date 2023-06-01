d36ea8beecdc2eacf2b2ebe45bd7b89b54f9d562
G284122022 <105191625+G284122022@users.noreply.github.com>
2023-06-01 15:32:17 +0900

    プロジェクト課題途中過程

--- project.js
+++ project.js
@@ -1,9 +1,61 @@
 let submit = document.querySelector("button#submit");
-submit.addEventListener("click", getData);
+submit.addEventListener("click", sendRequest);
 
-function getData() {
+function sendRequest() {
     let input = document.querySelector("select#country");
-    console.log(input.value);
-    // let display = document.querySelector("div#display");
-    // display.textContent = input.value;
+    let id = input.value;
+
+    // URL を設定
+    let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/" + id + ".json";
+
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
+}
+
+// 通信が成功した時の処理
+function showResult(resp) {
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
+
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    // ここから記入
+    let input = document.querySelector("select#country");
+    let id = input.value;
+    let lat = document.querySelector("td#lat");
+    lat.textContent = data.coord.lat;
+    let lon = document.querySelector("td#lon");
+    lon.textContent = data.coord.lon;
+    let weather = document.querySelector("td#weather");
+    weather.textContent = data.weather.description;
+    let min = document.querySelector("td#min");
+    min.textContent = data.main.temp_min;
+    let max = document.querySelector("td#max");
+    max.textContent = data.main.temp_max;
+    let hum = document.querySelector("td#hum");
+    hum.textContent = data.main.humidity;
+    let sp = document.querySelector("td#sp");
+    sp.textContent = data.wind.speed;
+    let di = document.querySelector("td#di");
+    di.textContent = data.wind.deg;
+    let name = document.querySelector("td#name");
+    name.textContent = data.name;
+
+
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+    console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+    console.log('Ajax 通信が終わりました');
 }
